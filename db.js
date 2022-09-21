0f4caf26059a02556f6f09ea2d1a085b721abcfa
MKTraore94 <moussakaramokotraore@gmail.com>
2022-09-21 11:12:01 +0000

    first commit

---
+++ db.js
@@ -0,0 +1,17 @@
+const db = require('./data/db.json');
+const products = require('./data/products.json');
+const signupUsers = require('./data/signupUsers.json');
+const cart = require('./data/cart.json');
+const commentaires = require('./data/commentaires.json');
+const wishlist = require('./data/wishlist.json');
+
+module.exports = {
+    db,
+    products,
+    signupUsers,
+    cart,
+    commentaires,
+    wishlist,
+
+
+}
